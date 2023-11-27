8d33a0fe8a331bd8c28888255fd00d24aaebd581
zuchalbastian <assasinbastian022@gmail.com>
2023-11-27 11:42:39 +0700

    feat: sample login test, interacting with element, timeouts, find not working

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -10,3 +10,19 @@ it('Google Test', function () {
     cy.wait(3000)
     cy.contains('Video').click()
 })
+
+it.only('login test', function () {
+
+    //.should('be.visible') -> Wait for an element to be visible
+    
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click().should('be.visible')
+
+    
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click().should('be.visible')
+    cy.get('.orangehrm-header-container > .oxd-button').click().should('be.visible')
+    // cy.get('.orangehrm-card-container').find('.oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space').click() -> FAIL because in web haven't id
+    cy.contains('Save').click().should('be.visible')
+})
