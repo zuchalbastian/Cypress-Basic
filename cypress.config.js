c943662efc8670329f8104cd3add93e7b7605fce
zuchalbastian <assasinbastian022@gmail.com>
2023-11-27 10:16:49 +0700

    fix: issue: timed out retrying after 4000ms: Expected to find element, added defaultCommandTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
+    defaultCommandTimeout: 4000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -4,5 +4,7 @@ it('Google Test', function () {
 
     cy.visit('https://www.google.com/')
 
-    cy.get('#APjFqb').type('Naruto Shippuden{enter}')
+    cy.get('#APjFqb').type('Automation Step By Step{enter}')
+
+    cy.get(':nth-child(4) > .cIkxbf > .usJj9c > :nth-child(1) > .VttTV > :nth-child(1) > h3 > .l').click()
 })
